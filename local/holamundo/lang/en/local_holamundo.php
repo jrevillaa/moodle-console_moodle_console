0a200e2a4c51647227333999e18a0b547beeaf85
Jair Revilla <jair@atypax.com>
2016-10-18 16:35:46 -0500

    second commit 2

--- local/holamundo/lang/en/local_holamundo.php
+++
@@ -1,6 +0,0 @@
-<?php
-    defined("MOODLE_INTERNAL") || die();
-   $string["modulename"] = "holamundo";
-   $string["exampletitle"] = "Link Example";
-   $string["menutitleexample"] = "Page Example";
-   $string["modulenameplural"] = "holamundos";
